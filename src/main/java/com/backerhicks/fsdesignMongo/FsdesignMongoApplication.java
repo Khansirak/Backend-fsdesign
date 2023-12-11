e89516f75c09b2703f727d6d85f7093b8bbb91cf
Khansirak <khan.sirak@bakerhicks.at>
2023-12-11 18:11:44 +0100

    The project

---
+++ src/main/java/com/backerhicks/fsdesignMongo/FsdesignMongoApplication.java
@@ -0,0 +1,13 @@
+package com.backerhicks.fsdesignMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsdesignMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsdesignMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backerhicks/fsdesignMongo/FsdesignMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backerhicks.fsdesignMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsdesignMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
